ea53bef8775a9c488578bda69482c5b695532248
Liu Yiqun <liuyiqun01@baidu.com>
2025-09-03 10:33:12 +0800

    Fix several ops.

--- athena/generators/autogen_op_name_and_arg_names.py
+++ athena/generators/autogen_op_name_and_arg_names.py
@@ -740,7 +740,6 @@ phi_ops_yaml_ops = [
         ("Tensor", "x"),
         ("Scalar(int64_t)", "axis"),
         ("bool", "keepdims"),
-        ("bool", "flatten"),
         ("DataType", "dtype"),
     ),
     (
@@ -1960,6 +1959,12 @@ phi_ops_yaml_ops = [
         ("float", "scale"),
     ),
     (
+        "masked_fill",
+        ("Tensor", "x"),
+        ("Tensor", "mask"),
+        ("Tensor", "value"),
+    ),
+    (
         "masked_multihead_attention_",
         ("Tensor", "x"),
         ("Tensor", "cache_kv"),
@@ -2378,12 +2383,19 @@ phi_ops_yaml_ops = [
     ("relu", ("Tensor", "x")),
     ("relu6", ("Tensor", "x")),
     ("renorm", ("Tensor", "x"), ("float", "p"), ("int", "axis"), ("float", "max_norm")),
-    ("repeat_interleave", ("Tensor", "x"), ("int", "repeats"), ("int", "axis")),
+    (
+        "repeat_interleave",
+        ("Tensor", "x"),
+        ("int", "repeats"),
+        ("int", "axis"),
+        ("int", "output_size"),
+    ),
     (
         "repeat_interleave_with_tensor_index",
         ("Tensor", "x"),
         ("Tensor", "repeats"),
         ("int", "axis"),
+        ("int", "output_size"),
     ),
     ("reverse", ("Tensor", "x"), ("IntArray", "axis")),
     (
