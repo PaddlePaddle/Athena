903bd7de284da4d358277c13e19c7ba690be0cae
HeyDavid633 <wenhao.dai633@gmail.com>
2025-09-04 18:48:24 +0800

    fix bug for TrainIR2InferIR

--- athena/util/input_output_tensors_extractor.py
+++ athena/util/input_output_tensors_extractor.py
@@ -6,8 +6,8 @@ class InputOutputTensorsExtractor:
         self.block_func = block_func
         self.input_tensors = []
         self.output_tensors = []
-        self.op_id2depth = defaultdict(int)
         self.tensor_name2depth = defaultdict(int)
+        self.tensor_name2ancestors = defaultdict(set)
 
     def Extract(self, free_vars, args, max_depth_output_only=False):
         self.input_tensors += list(free_vars)
@@ -15,13 +15,13 @@ class InputOutputTensorsExtractor:
         self.block_func(self, *free_vars)(*args)
 
         if max_depth_output_only:
-            max_output_tensor_depth = max(
-                self.tensor_name2depth[tensor.name] for tensor in self.output_tensors
+            ancestors = set(
+                ancestor
+                for tensor in self.output_tensors
+                for ancestor in self.tensor_name2ancestors[tensor.name]
             )
             self.output_tensors = [
-                tensor
-                for tensor in self.output_tensors
-                if self.tensor_name2depth[tensor.name] == max_output_tensor_depth
+                tensor for tensor in self.output_tensors if tensor.name not in ancestors
             ]
 
         return self.input_tensors, self.output_tensors
@@ -53,20 +53,20 @@ class InputOutputTensorsExtractor:
             getattr(self, method_name)(op, *input_tensors)
 
         ret = op.GetResults()
-
         valid_input_tensors = [t for t in input_tensors if t is not None]
 
         if valid_input_tensors:
-            input_depth = max(
-                (self.tensor_name2depth[tensor.name] for tensor in valid_input_tensors),
-                default=0,
+            input_ancestors = set(
+                ancestor
+                for tensor in valid_input_tensors
+                for ancestor in self.tensor_name2ancestors[tensor.name]
             )
         else:
-            input_depth = 0
+            input_ancestors = set()
 
-        op_depth = input_depth + 1
-        self.op_id2depth[op.op_id] = op_depth
         for tensor in ret:
-            self.tensor_name2depth[tensor.name] = op_depth
+            self.tensor_name2ancestors[tensor.name] = input_ancestors | {
+                t.name for t in valid_input_tensors
+            }
 
         return ret
